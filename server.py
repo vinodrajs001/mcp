96d9c1f85d3db48a00f6dee9deeed3268a3c7153
vinod s <vinodshal237@gmail.com>
2026-08-26 06:34:11 +0530

    test

--- server.py
+++ server.py
@@ -1,11 +1,7 @@
 from fastmcp import FastMCP
 
-mcp = FastMCP("My MCP Server")
+mcp = FastMCP("MyServer")
 
 @mcp.tool
-def greet(name: str) -> str:
+def hello(name: str) -> str:
     return f"Hello, {name}!"
-
-if __name__ == "__main__":
-    mcp.run()
-
