ef45bb5625fb75c592b36558c2e6b968ce84bc22
vinod s <vinodshal237@gmail.com>
2026-08-26 09:57:28 +0530

    changes

--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
 fastmcp run server.py:mcp --transport http --port 8000
+
+https://horizon.prefect.io/vinod-corp/servers?q=&types=&sort=recent
+
+<!-- command line to execute -->
+mcp dev server.py 
--- server.py
+++ server.py
@@ -1,7 +1,12 @@
 from fastmcp import FastMCP
+from mcp.server.fastmcp import FastMCP
 
 mcp = FastMCP("MyServer")
 
-@mcp.tool
+@mcp.tool()
 def hello(name: str) -> str:
     return f"Hello, {name}!"
+
+
+if __name__ == "__main__":
+    mcp.run()
